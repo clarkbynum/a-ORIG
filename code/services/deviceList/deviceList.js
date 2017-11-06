c7208a58bafb45c8209dbeaf1a68b2c8a1600767
Clark Bynum <sh3nan1gans@users.noreply.github.com>
2017-11-06 15:18:49 -0600

    created by sh3nan1gans

---
+++ code/services/deviceList/deviceList.js
@@ -0,0 +1,50 @@
+function deviceList(req, resp){
+    resp.success([
+  {
+    "label": "Thunder React #16823",
+    "value": {
+      "allow_certificate_auth": true,
+      "allow_key_auth": true,
+      "battery": "98",
+      "causetrigger": null,
+      "certificate": "",
+      "created_date": 1502368760,
+      "description": "ThunderReact Board",
+      "device_key": "b6cfe2980b90c4b1e4e2a5d3ba7d :: 16823",
+      "device_label": "Thunder React #16823",
+      "enabled": true,
+      "gps_coordinates": "{\"markers\":[{\"lat\": 37.4047, \"lng\": -121.97521}], \"zoom\": 14}",
+      "last_active_date": 1502368760,
+      "location": "103 E. 5th Austin, TX 78701",
+      "map_url": "http://thunderblade.local/www/austinGmap.png",
+      "name": "16823",
+      "state": "disconnected",
+      "system_key": "b6cfe2980b90c4b1e4e2a5d3ba7d",
+      "type": "Prototype SiLabs Thunderboard"
+    }
+  },
+  {
+    "label": "Thunder Sense #31322",
+    "value": {
+      "allow_certificate_auth": true,
+      "allow_key_auth": true,
+      "battery": "100",
+      "causetrigger": null,
+      "certificate": "",
+      "created_date": 1502368761,
+      "description": "Brent's Sensor",
+      "device_key": "b6cfe2980b90c4b1e4e2a5d3ba7d :: 1735258",
+      "device_label": "Thunder Sense #31322",
+      "enabled": true,
+      "gps_coordinates": "{\"markers\":[{\"lat\": 33.7817936, \"lng\": -84.3893761}], \"zoom\": 14}",
+      "last_active_date": 1502368761,
+      "location": "1950 Eisenhower Boulevard, Fort Lauderdale, FL 33316",
+      "map_url": "http://thunderblade.local/www/sfGmap.png",
+      "name": "1735258",
+      "state": "connected",
+      "system_key": "b6cfe2980b90c4b1e4e2a5d3ba7d",
+      "type": "Thunderboard Sense"
+    }
+  },
+])
+}
